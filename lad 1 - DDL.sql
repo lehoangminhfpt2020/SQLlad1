ea873365679cc4a21f2aaa6c5c4aa1f6b7cbe5e1
lehoangminhfpt2020 <65264453+lehoangminhfpt2020@users.noreply.github.com>
2020-08-10 22:49:32 +0700

    Add files via upload

---
+++ lad 1 - DDL.sql
@@ -0,0 +1,54 @@
+﻿--Data Definition Language (DDL) là ngôn ngữ định nghĩa dữ liệu 
+
+-- Tạo cơ sở dữ liệu:
+CREATE DATABASE lad1
+GO
+
+-- sử dụng cơ sở dữ liệu ttkh vừa tạo
+USE lad1
+GO
+
+-- Tạo bảng trong cơ sở dữ liệu
+CREATE TABLE thongtin
+(Mail VARCHAR(30),
+ Name VARCHAR(30),
+ TelephoneNumber INT)
+/* tạo 1 bảng có 3 trường là 
+Mail với kiểu dữ liệu là VARCHAR, kích thước 30
+Name kiểu dữ liệu NTEX
+TelephoneNumber kiểu INT
+  */
+
+-- Thêm một cột vào trong bảng đó
+ALTER TABLE thongtin ADD Address NVARCHAR(50)
+/* ALTER là chỉnh sửa 1 đối tượng
+   TABLE thongtin là bảng thongtin = đối tượng được chỉnh sửa
+   ADD Address NVARCHAR(50) là thêm một trường vào bảng với kiểu NVARCHAR gồm 50 ký tự
+   */
+
+-- Thêm dữ liệu vào bảng thông tin
+INSERT INTO thongtin values ('minh@yahoo.com','minhminh',9898,'Hà Nội')
+INSERT INTO thongtin values ('min11@yahoo.com','leee',342,'Hà Nột')
+INSERT INTO thongtin values ('mih22@yahoo.com','hoang',1198,'Hàm Nội')
+INSERT INTO thongtin values ('mi44@yahoo.com','mlemi',653,'Hà Lội')
+
+--Lấy ra tất cả các dữ liệu trong bảng
+SELECT * FROM thongtin
+
+--xóa bản ghi có Mail=minh@yahoo.com
+DELETE FROM thongtin WHERE Mail ='minh@yahoo.com'
+
+-- sửa bản ghi có Mail=mi44@yahoo.com
+UPDATE thongtin SET Name='okem' WHERE Mail='mi44@yahoo.com'
+
+-- Tạo login account để test GRANT
+CREATE LOGIN nguoi1 WITH PASSWORD='123456'
+
+--Tạo use từ login account vừa tạo 
+CREATE USER nguoi1 FROM LOGIN nguoi1
+
+-- Hủy bỏ tất cả các quyền của USER nguoi1 trên bảng thongtin
+REVOKE ALL ON thongtin FROM nguoi1
+
+-- Gán quyền SELECT cho nguoi1 trên bảng thongtin
+GRANT SELECT ON thongtin TO nguoi1
---
+++ lad 1.sql
@@ -0,0 +1,60 @@
+﻿
+USE AdventureWorks2019 -- sử dụng cơ sở dữ liệu Adventure Diagrams
+
+SELECT * FROM HumanResources.Employee --
+GO
+
+-- Khai báo biến
+--1 khai báo biến cục bộ
+DECLARE @bien1 INT -- Khai báo biến cục bộ tên là bien1 kiểu int 
+SELECT @bien1=1    -- Gán giá trị cho biến bằng 1
+SELECT GroupName FROM HumanResources.Department WHERE DepartmentID = @bien1
+GO 
+
+--2 khai báo biến toàn cục 
+SELECT @@LANGUAGE  -- Hiển thị thông tin của biến toàn cục @@LANGUAGE, trả lại thông tin ngôn ngữ sử dụng trong SQL server 
+SELECT @@DATEFIRST -- Hiển thị thông tin của biến toàn cục @@DATEFIRST, trả lại thông tin DATEFIRST sử dụng trong SQL server
+SELECT @@VERSION   -- Hiển thị thông tin của biến toàn cục @@VERSION, trả lại thông tin phiên bản version của SQL 
+
+--Hàm trong SQl 
+-- hàm Sum lấy tổng tất cả các giá trị các bản ghi trường StadardCost thuộc bảng ProductCostHistory
+SELECT SUM(StandardCost) FROM Production.ProductCostHistory
+
+-- Hàm AVG Tính giá trị trung bình của trường StadardCost thuộc bảng ProductCostHistory
+SELECT AVG(StandardCost) FROM Production.ProductCostHistory
+
+-- Hàm MAX,MIN lấy ra gái trị lớn nhất và nhỏ nhất của trường StadardCost thuộc bảng ProductCostHistory
+SELECT MAX(StandardCost) FROM Production.ProductCostHistory
+SELECT MIN(StandardCost) FROM Production.ProductCostHistory
+
+-- Hàm COUNT đếm tổng số bản ghi tỏng bảng Productphoto
+SELECT COUNT(*) FROM Production.ProductPhoto
+
+-- Hàm lấy ra ngày giờ hệ thống:
+SELECT GETDATE()
+
+-- HÀM lấy ra giờ, ngày, tháng, năm trong hệ thống:
+SELECT DATEPART(HH,GETDATE())
+SELECT DATEPART(DD,GETDATE())
+SELECT DATEPART(MM,GETDATE())
+SELECT DATEPART(YY,GETDATE())
+
+-- Hàm CONVERT dùng định dạng ngày
+SELECT CONVERT(nvarchar(50),GETDATE(),103)
+/* CONVERT(datatype(leght), expression(),Style)
+datatype là kiểu giữ liệu cần chuyển sang
+leght chỉ ra độ dài của dữ liệu 
+expressinon chỉ ra tên cột, hằng số, hàm biến hoặc là câu truy vấn con
+style chỉ ra kiểu hiển thị giá trị ngày tháng(dd/mm/yyyy hay mm/đ/yyyy)
+*/
+
+
+-- Lấy ra định danh của cơ sở dữ liệu
+SELECT DB_ID ('AventureWorks2019')
+
+
+
+
+
+
+
